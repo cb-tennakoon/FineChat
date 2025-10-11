3ddcf83119411676dc56d7c85c2cfd11c1b7ea0c
Chathuranga <cbthraeka1995@gmail.com>
2025-10-11 22:11:08 +0530

    connect mongodb database in local and day work

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS for your Next.js frontend
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
